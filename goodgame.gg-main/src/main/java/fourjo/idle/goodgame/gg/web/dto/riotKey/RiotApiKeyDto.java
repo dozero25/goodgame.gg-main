f027aef9b13609b817997a83ef9c0fab23ab627e
Yoon Do Yeong <ehdud05264@naver.com>
2025-04-21 08:12:19 +0900

    확인

---
+++ goodgame.gg-main/src/main/java/fourjo/idle/goodgame/gg/web/dto/riotKey/RiotApiKeyDto.java
@@ -0,0 +1,11 @@
+package fourjo.idle.goodgame.gg.web.dto.riotKey;
+
+import lombok.Data;
+
+@Data
+public class RiotApiKeyDto {
+
+    private final String mykey = "RGAPI-7171a9b8-8b67-4378-ad6c-53540dcf17d9";
+    private final String serverUrl = "https://kr.api.riotgames.com";
+    private final String severUrlAsia = "https://asia.api.riotgames.com";
+}
